993885117890cf9865d32fd4400f1a0c545f14b7
akshayDhingra01 <a4akshayd@gmail.com>
2022-10-14 20:17:44 +0530

    UPDATE: schemas in models

--- models/Cart.js
+++ models/Cart.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+const CartSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("Cart", CartSchema);
